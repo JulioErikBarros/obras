49b6e86966c6cd56b7971897864729dcf8fd05d4
Julio Barros <juliobeserra0@gmail.com>
2026-05-29 15:18:04 -0300

    Add files via upload

---
+++ backend/config/Database.php
@@ -0,0 +1,21 @@
+<?php
+class Database {
+    private $host = "localhost";
+    private $db_name = "controle_obras";
+    private $username = "root";
+    private $password = "";
+    public $conn;
+
+    public function getConnection() {
+        $this->conn = null;
+        try {
+            $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $this->conn->exec("set names utf8mb4");
+        } catch(PDOException $exception) {
+            echo "Erro na conexão com o banco de dados: " . $exception->getMessage();
+        }
+        return $this->conn;
+    }
+}
+?>
